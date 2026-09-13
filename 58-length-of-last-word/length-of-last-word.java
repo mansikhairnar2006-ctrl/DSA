cfc0c0212f444e4fa0eeec058e07e3349bdce3c1
mansikhairnar2006-ctrl <mansikhairnar2006@gmail.com>
2026-09-13 22:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (16.10%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int endIndex = s.length() - 1;
+    
+        while (endIndex >= 0 && s.charAt(endIndex) == ' ') {
+            endIndex--;
+        }
+      
+        int startIndex = endIndex;
+      
+        while (startIndex >= 0 && s.charAt(startIndex) != ' ') {
+            startIndex--;
+        }
+
+        return endIndex - startIndex;
+    }
+}
